4371a9648c20f8cd507031ff2ae9c064d7a3ed02
notemove <temp99182488@gmail.com>
2020-06-07 11:02:04 +0900

    データベースからcontentを取り出す

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,8 @@
 class PostsController < ApplicationController
   def index
     #  @をつけることでビューファイルで参照可能になる
-    @posts = [
-      "今日からProgateでRails",
-      "投稿一覧ページ作成中"
-    ]
+    # データベースからすべてのデータを取り出す
+    @posts = Post.all
+    
   end
 end
